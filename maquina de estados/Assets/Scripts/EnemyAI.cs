9ec361e96389ec252c33d68a33b4f6c2219466d8
Deimojuf1985 <33737666+Deimojuf1985@users.noreply.github.com>
2018-09-28 18:39:01 -0500

    maquina de estados
    subida de proyecto máquina de estados

---
+++ maquina de estados/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Enemy
+{
+    public class EnemyAI : MonoBehaviour
+    {
+        public NavMeshAgent navmeshagent;
+        public Transform target;
+
+        void Update()
+        {
+            Destino();
+        }
+
+        public void Destino()
+        {
+            navmeshagent.SetDestination(target.position);
+        }
+
+        public void Maweke()
+        {
+            navmeshagent = GetComponent<NavMeshAgent>();
+        }
+    }
+}
